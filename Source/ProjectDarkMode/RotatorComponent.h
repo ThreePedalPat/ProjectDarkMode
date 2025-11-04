af70564c9211b911234ed8ac6d2c3325caff2402
Pat <correale_colin@yahoo.com>
2025-11-04 12:12:36 -0800

    Valley environment work
    Lots of changes to models and textures, started adding goblin camp and mostly finished valley. Changed level flythrough to be more cinematic

---
+++ Source/ProjectDarkMode/RotatorComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameFramework/Actor.h"
+#include "RotatorComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJECTDARKMODE_API URotatorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	URotatorComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotator")
+	FRotator rot;
+	
+	AActor* myActor;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
